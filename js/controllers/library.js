c87e4364e3bc83459481a870ad76fd82c069f6d2
Adam <adam.gedney@gmail.com>
2014-03-28 11:21:04 -0400

    main li add icon triggering state change

--- js/controllers/library.js
+++ js/controllers/library.js
@@ -1,12 +1,54 @@
 var Library = (function(window, document, $){
 
 	//private vars
-	var _foo = 'bar';
+	var _addedToLibrary = false;
+
+
+
+
+
 
 	//constructor fuction
 	var library = function(){
 
-	};
+
+
+
+		//Add song to library========//
+		$(document).on('click', '.addToLibrary', function(event){
+			var id = $(this).attr('data-id');
+				this.toggle
+
+			//Handles adding and removing functions and button image swap
+			if(!this.toggle){
+				console.log(id, 'song id -song added to lib');
+
+				$(this).find('.add-icon').attr('src', 'images/icons/check.png');
+
+				this.toggle = !this.toggle;
+			}else{
+				console.log(id, 'song id -song REMOVED from lib');
+
+				$(this).find('.add-icon').attr('src', 'images/icons/add.png');
+
+				this.toggle = !this.toggle;
+			}
+
+
+		});
+
+
+
+
+
+
+	};//constructor function
+	//================================//
+
+
+
+
+
 
 	//methods and properties.
 	library.prototype = {
@@ -22,5 +64,22 @@ var Library = (function(window, document, $){
 
 	//return constructor
 	return library;
+
+
+
+
+
+
+
+
+
+//================================//
+//Class methods===================//
+//================================//
+
+
+
+
+
 
 })(window, document,jQuery);
