c1da076f318bf9c47852ac31300d8b437666f37d
Adam <adam.gedney@gmail.com>
2014-04-02 15:12:44 -0400

    querying youtube on refined tinysong data

--- api/app/controllers/SearchController.php
+++ api/app/controllers/SearchController.php
@@ -10,38 +10,63 @@ class SearchController extends BaseController {
 
 		//First checks the tinysong table in my DB for query.
 		//If found, display results, else hit the tinysong api & store result
-		// $queryLocalTinySong = $this->queryLocalTinySong($q);
-
-		// if($queryLocalTinySong == 'null response'){
-
-		// 	//Logic to check if search query was artist, album, or song title
-		// 	$response = json_decode($this->getGrooveshark($q));
-
-		// 	$what = $response;
-
-		// 	//Check if query was an artist name, album, or song
-		// 	if(strtolower($response[0]->ArtistName) == $q){
-		// 		$what = $response[0]->ArtistName . "  artist";
-
-		// 	}else if(strtolower($response[0]->AlbumName) == $q){
-		// 		$what = $response[0]->AlbumName . "  album";
-
-		// 	}else if(strtolower($response[0]->SongName) == $q){
-		// 		$what = $response[0]->SongName . "  song";
-		// 	}
-
-		// 	return $response;
-
-		// }else{
-
-		// 	return $queryLocalTinySong;
-		// }
-		$getYoutube = $this->getYoutube($q);
+		$queryLocalTinySong = $this->queryLocalTinySong($q);
 
 
+		if($queryLocalTinySong == 'null response'){
 
+			//Logic to check if search query was artist, album, or song title
+			$response = json_decode($this->getTinySong($q));
 
-		return $getYoutube;
+			$type = $response;
+
+			//Check if query was an artist name, album, or song
+			//Query YOUTUBE on this refined TinySong data
+			if(strtolower($response[0]->ArtistName) == $q){
+				$type = $response[0]->ArtistName . "  artist";
+
+				$ytResult = $this->getYoutube($type);
+
+			}else if(strtolower($response[0]->AlbumName) == $q){
+				$type = $response[0]->AlbumName . "  album";
+
+				$ytResult = $this->getYoutube($type);
+
+			}else if(strtolower($response[0]->SongName) == $q){
+				$type = $response[0]->SongName . "  song";
+
+				$ytResult = $this->getYoutube($type);
+			}
+
+			return $ytResult;
+
+		}else{
+
+			$response = $queryLocalTinySong;
+
+			$type = $response;
+
+			//Check if query was an artist name, album, or song
+			//Query YOUTUBE on this refined TinySong data
+			if(strtolower($response[0]->artist_name) == $q){
+				$type = $response[0]->artist_name;
+
+				$ytResult = $this->getYoutube($type);
+
+			}else if(strtolower($response[0]->album_name) == $q){
+				$type = $response[0]->album_name;
+
+				$ytResult = $this->getYoutube($type);
+
+			}else if(strtolower($response[0]->song_name) == $q){
+				$type = $response[0]->song_name;
+
+				$ytResult = $this->getYoutube($type);
+			}
+
+			return $ytResult;
+			// return $response;
+		}
 	}
 
 
@@ -52,10 +77,17 @@ class SearchController extends BaseController {
 	//================//
 	//Internal Methods//
 	//================//
+
+
 	public function logSearch()
 	{
 		return "Testing route";
 	}
+
+
+
+
+
 
 
 
@@ -68,23 +100,39 @@ class SearchController extends BaseController {
   		$YOUTUBE_API_KEY = 'AIzaSyCukRpGoGeXcvHKEEPRLKg7-toFMhtkeYk';
   		$MAX_RESULTS = 50;
 
+  		//Format string to strip spaces and add "+"
+		$queryExplode = explode(" ", $query);
+		$queryImplode = implode("+", $queryExplode);
 
+		$youtubeQuery = 'https://www.googleapis.com/youtube/v3/search?&maxResults=' . (string)$MAX_RESULTS . '&part=snippet&q=' . $queryImplode . '&regionCode=US&type=video&videoCategoryId=10&fields=items(etag,id(videoId),snippet(title,description,thumbnails))&key=' . (string)$YOUTUBE_API_KEY;
 
 		//returns the snippet and statistics part with only the id, desc, title, thumbnails fields
-		$youtubeResponse = file_get_contents('https://www.googleapis.com/youtube/v3/search?&maxResults=' . (string)$MAX_RESULTS . '&part=snippet&q=' . $query . '&regionCode=US&fields=items(id(videoId),snippet(title,description,thumbnails))&key=' . (string)$YOUTUBE_API_KEY);
+		$youtubeResponse = file_get_contents($youtubeQuery);
 
-		https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=led+zeppelin&regionCode=US&key=
+		//Insert query results into database for future searches
+		$inserted = YouTube::setResults($query, $youtubeResponse);
 
+		//Error handling for insert
+		if($inserted){
+			return $youtubeResponse;
 
+		}else{
 
-		return $youtubeResponse;
+			$error = array('error'=>'failed to insert youtube results into database');
+			return json_encode($error);
+		}
 
 	}
 
 
 
 
-	public function getGrooveshark($query){
+
+
+
+
+
+	public function getTinySong($query){
 
 		//Grooveshark API key
 		$TINY_API_KEY = '6ab1c1e7fdf25492f84948a6514238dc';
@@ -102,7 +150,6 @@ class SearchController extends BaseController {
 
 
 		//Insert query results into database for future searches
-		//$query, $url, $songId, $songName, $artistId, $artistName, $albumId, $albumName
 		$inserted = TinySong::setResults($query, $tinyResponse);
 
 
@@ -115,10 +162,6 @@ class SearchController extends BaseController {
 			$error = array('error'=>'failed to insert tinysong results into database');
 			return json_encode($error);
 		}
-
-
-
-
 	}
 
 
---
+++ api/app/models/YouTube.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class YouTube extends Eloquent{
+
+
+
+
+	//Set the query results to database
+	public static function setResults($query, $ytResponse)
+	{
+
+		//Loop through tinysong reult json and insert into database
+		foreach(json_decode($ytResponse) as $result){
+
+			var_dump($result);
+
+			// $insert = DB::table('youtube_results')->insert(array(
+			// 	'query' => $query,
+			// 	'etag' => $result->etag,
+			// 	'video_id' => $result->id->videoId,
+			// 	'title' => $result->snippet->title,
+			// 	'description' => $result->snippet->description,
+			// 	'img_default' => $result->snippet->thumbnails->default->url,
+			// 	'img_medium' => $result->snippet->thumbnails->medium->url,
+			// 	'img_high' => $result->snippet->thumbnails->high->url));
+
+		}
+
+		//Return boolean for success/fail
+		// return $insert;
+		// return $insert;
+	}
+
+
+
+
+
+	//Get the results for a query from database
+	public static function getResults($query)
+	{
+		$resultArray = array();
+
+		//Query DB on "query"
+		$results = DB::table('youtube_results')->where('query', '=', $query)->get();
+
+		foreach($results as $r){
+			array_push($resultArray, $r);
+		}
+
+		return $resultArray;
+	}
+
+
+}
